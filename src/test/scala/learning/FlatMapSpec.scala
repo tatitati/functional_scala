61ea0cd4daa69e300862d45d8ec553034ab48dbb
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-27 22:53:55 +0100

    added some flatmap

--- src/test/scala/learning/FlatMapSpec.scala
+++ src/test/scala/learning/FlatMapSpec.scala
@@ -4,4 +4,20 @@ import org.scalatest.FunSuite
 
 class FlatMapSpec extends FunSuite {
 
+  test("flatmap") {
+    def isPar(a: Int): Option[Int] = {
+      a % 2 match {
+        case 0 => Some(a)
+        case _ => None
+      }
+    }
+
+    val l = List(1,2,3,4,5)
+
+    val result1 = l.map( x => isPar(x) )
+    val result2 = l.flatMap( x => isPar(x) )
+
+    assert(List(None, Some(2), None, Some(4), None) === result1)
+    assert(List(2, 4) === result2)
+  }
 }
