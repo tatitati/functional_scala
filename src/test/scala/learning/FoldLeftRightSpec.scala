be33b03629fceeb9836939135c179f568df10254
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-27 23:54:03 +0100

    more

--- src/test/scala/learning/FoldLeftRightSpec.scala
+++ src/test/scala/learning/FoldLeftRightSpec.scala
@@ -3,5 +3,12 @@ package learning
 import org.scalatest.FunSuite
 
 class FoldLeftRightSpec extends FunSuite {
+  test("foldleft") {
+    val prices: Seq[String] = Seq("a", "b", "c")
+    val result1 = prices.foldLeft("____")(_ + _)
+    val result2 = prices.foldRight("____")(_ + _)
 
+    assert("____abc" === result1)
+    assert("abc____" === result2)
+  }
 }
