c38f3bfe36b7b3eda4d23021fd25a0c1b29ddd82
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-28 23:09:41 +0100

    adsf

--- src/test/scala/cats/CatMonadSpec.scala
+++ src/test/scala/cats/CatMonadSpec.scala
@@ -15,15 +15,46 @@ class CatMonadSpec extends FunSuite {
 //    def map[A, B](value: F[A])(func: A => B): F[B]
 //  }
 
-  test("pure()") {
+
+  // WITH OPTION
+
+  test("Option: pure()") {
     val result1 = Monad[Option].pure(3)
     assert(result1 === Some(3))
   }
 
-  test("flatMap()") {
+  test("Option: flatMap()") {
     val result1 = Monad[Option]
       .flatMap(Some(3))(a => Some(a + 2))
 
     assert(result1 === Some(5))
+  }
+
+  test("Option: map()") {
+    var result1 = Monad[Option]
+      .map(Some(3))(a => 2*a)
+
+    assert(result1 === Some(6))
+  }
+
+  // with LIST
+
+  test("LIST: pure()") {
+    val result1 = Monad[List].pure(3)
+    assert(result1 === List(3))
+  }
+
+  test("LIST: flatMap()") {
+    val result1 = Monad[List]
+      .flatMap(List(3))(a => List(a, a*10))
+
+    assert(result1 === List(3, 30))
+  }
+
+  test("LIST: map()") {
+    val result1 = Monad[List]
+      .map(List(3))(a => a + 10)
+
+    assert(result1 === List(13))
   }
 }
