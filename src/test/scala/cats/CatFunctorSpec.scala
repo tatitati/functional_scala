8befa356798ee2f16fa6ec982c51e708aabd51ca
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-28 14:08:15 +0100

    cat functor

--- src/test/scala/cats/CatFunctorSpec.scala
+++ src/test/scala/cats/CatFunctorSpec.scala
@@ -7,9 +7,19 @@ import cats.instances.option._
 
 class CatFunctorSpec extends FunSuite {
 
-  test("functor in cats") {
+  test("functor list in cats") {
     val list1 = List(1,2,3)
+
     val list2 = Functor[List].map(list1)(_ * 2)
+
     assert(List(2,4,6) === list2)
+  }
+
+  test("functor option in cats") {
+    val opt1 = Option(1,2,3)
+
+    val opt2 = Functor[Option].map(opt1)(_.toString)
+
+    assert(Some("(1,2,3)") === opt2)
   }
 }
