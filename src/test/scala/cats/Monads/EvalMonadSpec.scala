3099844df0fdba67a9836c3fa9e4c8efefe612e8
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-30 23:45:34 +0100

    more

--- src/test/scala/cats/Monads/EvalMonadSpec.scala
+++ src/test/scala/cats/Monads/EvalMonadSpec.scala
@@ -24,9 +24,37 @@ class EvalMonadSpec
     val always = Eval.always(math.random.toInt + 2000)
 
     assert(
-      now.isInstanceOf[Eval[Int]] &&
+        now.isInstanceOf[Eval[Int]] &&
         later.isInstanceOf[Eval[Int]] &&
         always.isInstanceOf[Eval[Int]]
     )
+  }
+
+  test("use case") {
+    import cats.syntax.flatMap._
+    import cats.syntax.functor._
+
+
+    // bound context here:
+    // we are saying that for F[_] we have an implicit Monad[F[_]]
+    def sumSquare[F[_] : Monad](a: F[Int], b: F[Int]): F[Int] = {
+      for {
+        x <- a
+        y <- b
+      } yield x*x + y*y
+    }
+
+
+    val result1 = sumSquare(Option(3), Option(4))
+    val result2 = sumSquare(List(3), List(4))
+    val result3 = sumSquare(3 : Id[Int], 4 : Id[Int])
+    val result4 = sumSquare(Monad[Id].pure(3), Monad[Id].pure(4))
+    val result5 = sumSquare(Eval.now(3), Eval.later(4))
+
+    assert(Some(25) === result1)
+    assert(List(25) === result2)
+    assert(25 === result3)
+    assert(25 === result4)
+    assert(25 === result5.value)
   }
 }
