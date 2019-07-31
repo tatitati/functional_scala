f2bc1ac5dbafabab9b89168e59cd8508a8bf4859
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-31 20:35:30 +0100

    more

--- src/test/scala/cats/Monads/StateMonadSpec.scala
+++ src/test/scala/cats/Monads/StateMonadSpec.scala
@@ -21,4 +21,28 @@ class StateMonadSpec extends FunSuite{
       val result3 = a.runA(10).value
       assert("value is: 10" === result3)
   }
+
+  test("composing with state monad") {
+    val step1 = State[Int, String] {
+      num =>
+        val ans = num + 1
+        (ans, s"From step 1: $ans")
+    }
+
+    val step2 = State[Int, String] {
+      num =>
+        val ans = num * 2
+        (ans, s"From step 2: $ans")
+    }
+
+    val both = for {
+      a <- step1
+      b <- step2
+    } yield (a, b)
+
+    val (state, result) = both.run(20).value
+
+    assert(42 === state)
+    assert(("From step 1: 21", "From step 2: 42") === result)
+  }
 }
