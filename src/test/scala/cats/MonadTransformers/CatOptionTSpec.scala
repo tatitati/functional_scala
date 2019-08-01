bac52308a41936f2b224c42625c2105f810831a2
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-01 22:29:54 +0100

    asdoifasdfa

--- src/test/scala/cats/MonadTransformers/CatOptionTSpec.scala
+++ src/test/scala/cats/MonadTransformers/CatOptionTSpec.scala
@@ -9,11 +9,10 @@ class CatOptionTSpec extends FunSuite {
 
   type ListOption[A] = OptionT[List, A]
 
-  test("OptionT") {
+  test("can create OptionT in two ways") {
     val a = 32.pure[ListOption]
     val b = OptionT(List(Option(32)))
 
     assert(a == b)
-
   }
 }
