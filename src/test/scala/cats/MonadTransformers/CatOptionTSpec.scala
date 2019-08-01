7a150fc25f93f26cc7c59146ad122e1c9f659503
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-01 22:13:57 +0100

    more

---
+++ src/test/scala/cats/MonadTransformers/CatOptionTSpec.scala
@@ -0,0 +1,19 @@
+package test.cats.MonadTransformers
+
+import cats.data.OptionT
+import cats.instances.list._
+import cats.syntax.applicative._
+import org.scalatest.FunSuite
+
+class CatOptionTSpec extends FunSuite {
+
+  type ListOption[A] = OptionT[List, A]
+
+  test("OptionT") {
+    val a = 32.pure[ListOption]
+    val b = OptionT(List(Option(32)))
+
+    assert(a == b)
+
+  }
+}
