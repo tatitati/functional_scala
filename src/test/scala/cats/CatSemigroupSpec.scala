8944dc757514d5b6c251cefdd1ce0675a2d3fa13
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-03 21:24:55 +0100

    more

--- src/test/scala/cats/CatSemigroupSpec.scala
+++ src/test/scala/cats/CatSemigroupSpec.scala
@@ -5,11 +5,6 @@ import cats.Semigroup
 import cats.implicits._
 
 class CatSemigroupSpec extends FunSuite {
-  // Definition:
-  //
-  // trait Semigroupal[F[_]] {
-  //  def product[A, B](fa: F[A], fb: F[B]): F[(A, B]
-  // }
 
   test("semigroup") {
     val result = Semigroup[String].combine("hi", "there")
---
+++ src/test/scala/cats/CatSemigroupalSpec.scala
@@ -0,0 +1,13 @@
+package cats
+
+import org.scalatest.FunSuite
+
+class CatSemigroupalSpec extends FunSuite {
+  // Definition:
+  //
+  // trait Semigroupal[F[_]] {
+  //  def product[A, B](fa: F[A], fb: F[B]): F[(A, B]
+  // }
+
+  
+}
