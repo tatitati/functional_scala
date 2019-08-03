c359e04637b79d0b23049fe992a186081a83a4be
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-03 10:15:49 +0100

    more

--- src/test/scala/cats/Monads/OptionMonadSpec.scala
+++ src/test/scala/cats/Monads/OptionMonadSpec.scala
@@ -1,6 +1,7 @@
 package test.cats.Monads
 
-import cats.Monad
+import cats.{Eval, Id, Monad}
+import cats.instances.option._
 import org.scalatest.FunSuite
 
 class OptionMonadSpec extends FunSuite {
@@ -12,20 +13,23 @@ class OptionMonadSpec extends FunSuite {
   //  }
 
   test("pure()") {
-    val result1 = Monad[Option].monadOption.pure(3)  //monad option is instance of cats.instances.OptionInstances$$anon$1@376c94a8
+    val result1 = Monad[Option].pure(3)  //monad option is instance of cats.instances.OptionInstances$$anon$1@376c94a8
     assert(Some(3) === result1)
   }
-  
+
   test("map()") {
     val result1 = Monad[Option].map(Some(3))(a => 2*a)
+    val result2 = Monad[Option].map(Some(3))(a => Some(a.toString))
+
     assert(Some(6) === result1)
+    assert(Some(Some("3")) === result2)
   }
 
   test("flatMap()") {
-    val result1 = Monad[Option].flatMap(Some(3))(a => Some(a + 2))
+    val result1 = Monad[Option].flatMap(Some(3))(a => Some(2*a))
     val result2 = Monad[Option].flatMap(Some(3))(a => Some(a.toString))
 
-    assert(Some(5) === result1)
+    assert(Some(6) === result1)
     assert(Some("3") === result2)
   }
 
@@ -58,21 +62,21 @@ class OptionMonadSpec extends FunSuite {
     assert(Some(7) === result4)
   }
 
-  test("use case") {
-    import cats.syntax.flatMap._
-    import cats.syntax.functor._
-
-
-    // bound context here:
-    // we are saying that for F[_] we have an implicit Monad[F[_]]
-    def sumSquare[F[_] : Monad](a: F[Int], b: F[Int]): F[Int] = {
-      for {
-        x <- a
-        y <- b
-      } yield x*x + y*y
-    }
-
-    val result1 = sumSquare(Option(3), Option(4))
-    assert(Some(25) === result1)
-  }
+//  test("use case") {
+//    import cats.syntax.flatMap._
+//    import cats.syntax.functor._
+//
+//
+//    // bound context here:
+//    // we are saying that for F[_] we have an implicit Monad[F[_]]
+//    def sumSquare[F[_] : Monad](a: F[Int], b: F[Int]): F[Int] = {
+//      for {
+//        x <- a
+//        y <- b
+//      } yield x*x + y*y
+//    }
+//
+//    val result1 = sumSquare(Option(3), Option(4))
+//    assert(Some(25) === result1)
+//  }
 }
