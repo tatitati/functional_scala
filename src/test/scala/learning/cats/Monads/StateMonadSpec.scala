e9c518cd40c2010edf7221f3548f901142c5bec6
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-04 23:01:39 +0100

    more

--- src/test/scala/learning/cats/Monads/StateMonadSpec.scala
+++ src/test/scala/learning/cats/Monads/StateMonadSpec.scala
@@ -5,38 +5,54 @@ import org.scalatest.FunSuite
 
 class StateMonadSpec extends FunSuite{
 
+  test("default behaviour") {
+    val result = State.get[Int].run(10).value
+    assert((10, 10) === result)
+  }
+
+  test("modify input") {
+    val a = State[Int, String] { input =>
+      val newvalue = input*2
+      (newvalue, s"value is: $newvalue")
+    }
+
+    val result1 = a.run(10).value
+    assert((20, "value is: 20") === result1)
+  }
+
   test("run(), runS(), runA()") {
       val a = State[Int, String] { x =>
         (x, s"value is: $x")
       }
 
-      val(state1, result1) = a.run(10).value
-      val state2 = a.runS(10).value
+      val result1 = a.run(10).value
+      val result2 = a.runS(10).value
       val result3 = a.runA(10).value
 
-      assert(10 === state1 && "value is: 10" === result1)
-      assert(10 === state2)
+      assert((10, "value is: 10") === result1)
+      assert(10 === result2)
       assert("value is: 10" === result3)
   }
 
   test("composing") {
-    val step1 = State[Int, String] { num =>
-      (num + 1, s"From step 1: ${num + 1}")
+    val step1 = State[Int, String] { input =>
+      (input + 1, s"From step 1: ${input + 1}")
     }
 
-    val step2 = State[Int, String] { num =>
-      (num * 2, s"From step 2: ${num * 2}")
+    val step2 = State[Int, String] { input =>
+      (input * 2, s"From step 2: ${input * 2}")
     }
 
     val both = for {
-      a <- step1
-      b <- step2
+      a <- step1  // +1
+      b <- step2  // *2
     } yield (a, b)
 
-    val (state, result) = both.run(20).value
+    val result = both.run(20).value
 
-    assert(42 === state)
-    assert(("From step 1: 21", "From step 2: 42") === result)
+    assert(
+      (42, ("From step 1: 21", "From step 2: 42")) === result
+    )
   }
 
   test("use case") {
