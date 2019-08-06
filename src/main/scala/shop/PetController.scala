2119ba31b9be23c44e001f8c26db8e80a3607d79
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-06 21:26:56 +0100

    simpler

--- src/main/scala/shop/PetController.scala
+++
@@ -1,5 +0,0 @@
-package shop
-
-class PetController {
-
-}
--- src/main/scala/shop/PetService.scala
+++ src/main/scala/shop/PetService.scala
@@ -5,7 +5,7 @@ class PetService(petRepository: PetRepository) {
   def create(pet: Pet) = {
     for {
       _ <- petRepository.doesNotExist(pet)
-      _ <- petRepository.create(pet)
-    }
+      saved <- petRepository.create(pet)
+    } yield saved
   }
 }
