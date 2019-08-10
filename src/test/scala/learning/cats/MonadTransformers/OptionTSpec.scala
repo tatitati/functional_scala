11703efb534e94e2f9cc57fe07256ae8fad931b3
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-10 18:49:55 +0100

    more

--- src/test/scala/learning/cats/MonadTransformers/OptionTSpec.scala
+++ src/test/scala/learning/cats/MonadTransformers/OptionTSpec.scala
@@ -1,6 +1,6 @@
 package test.learning.cats.MonadTransformers
 
-import cats.data.OptionT
+import cats.data.{EitherT, OptionT}
 import cats.instances.list._
 import cats.syntax.applicative._
 import org.scalatest.FunSuite
@@ -18,13 +18,33 @@ class OptionTSpec extends FunSuite {
     assert(a == b)
   }
 
-  test("Monad transformers are simply nesting flatMaps") {
+  test("Monad transformers are simply nesting flatMaps, so the access without needing nested loops to the value") {
     val a = OptionT(List(Option(32)))
+    //    we have:
+    //        OptionT
+    //          List
+    //            Option
 
     for{
       aVal <- a
     } yield {
-      assert(32 === aVal )
+      assert(32 === aVal , "Monad transformer cross two layers of monads in once (List and Option in here)")
     }
   }
+
+  test("whatever") {
+    import cats.instances.either._
+    
+    type ErrorOr[A] = Either[String, A]
+    type ErrorOrOption[A] = OptionT[ErrorOr, A]
+
+    //  we have:
+    //        OptionT
+    //          Either
+    //            Option
+
+    val a = 10.pure[ErrorOrOption]
+    println(a)
+  }
+
 }
