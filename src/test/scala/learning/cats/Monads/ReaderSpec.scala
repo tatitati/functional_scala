c853a176ebb876522b744dd69c7599b7a02980a1
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-11 00:56:59 +0100

    more

--- src/test/scala/learning/cats/Monads/ReaderSpec.scala
+++ src/test/scala/learning/cats/Monads/ReaderSpec.scala
@@ -1,7 +1,26 @@
 package test.learning.cats.Monads
 
+import cats.data.Reader
 import org.scalatest.FunSuite
 
 class ReaderSpec extends FunSuite {
 
+    test("") {
+        case class User(name: String)
+        case class Account(identifier: Int)
+        case class DbConnection(ip: String)
+
+        def createUserReader(u: User): Reader[DbConnection, Int] = Reader { _ => 0}
+        def createAccountReader(a: Account): Reader[DbConnection, Int] = Reader { _ => 1}
+        def registerNewUserReader(name: String): Reader[DbConnection, Int] = {
+          createUserReader(User(name)).flatMap { id =>
+            createAccountReader(Account(id))
+          }
+        }
+
+        val register: Reader[DbConnection, Int] = registerNewUserReader("John")
+        val accountId = register(DbConnection("122.33.222.11"))
+
+        assert(1 === accountId)
+    }
 }
