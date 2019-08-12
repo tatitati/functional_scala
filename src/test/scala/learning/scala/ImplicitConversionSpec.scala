ce8229ce756c1af16fd942e7c3537bd11fcbea5a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-12 23:08:13 +0100

    more

--- src/test/scala/learning/scala/ImplicitConversionSpec.scala
+++ src/test/scala/learning/scala/ImplicitConversionSpec.scala
@@ -6,12 +6,18 @@ class ImplicitConversionSpec extends FunSuite {
 
   test("I can add a new method to another class LIFTING OR WRAPPING") {
     case class Person(name: String) {
-      def greet(): String = s"Hello $name"
+      def greet(): String = s"Hello person: $name"
+    }
+
+    case class Counter(number: Int) {
+      def greet(): String = s"Hello number: $number"
     }
 
     implicit def liftStringToperson(str: String): Person = Person(str)
+    implicit def liftIntToperson(number: Int): Counter = Counter(number)
 
-    assert("Hello Maria" === "Maria".greet())
+    assert("Hello person: Maria" === "Maria".greet())
+    assert("Hello number: 5" === 5.greet())
   }
 
   test("I can use a shourtcut to do the same") {
