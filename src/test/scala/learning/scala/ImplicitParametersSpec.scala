fe5890ec3007343cda2e48b7ed3c9927572c8fe3
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-12 22:42:29 +0100

    implicits battle...

--- src/test/scala/learning/scala/ImplicitParametersSpec.scala
+++ src/test/scala/learning/scala/ImplicitParametersSpec.scala
@@ -11,4 +11,27 @@ class ImplicitParametersSpec extends FunSuite {
 
     assert("Hello Antonio" === sayHello)
   }
+
+  test("Type classes") {
+      case class User(name: String, age: Int)
+
+      trait InfoPrinter[T]{
+        def toInfo(value: T): String
+      }
+
+      implicit val stringPrinter = new InfoPrinter[String] {
+        override def toInfo(value: String): String = s"[__STRING__] $value"
+      }
+
+      implicit val userPrinter = new InfoPrinter[User] {
+        override def toInfo(value: User): String = s"[__USER__] ${value.name}, ${value.age}"
+      }
+
+      def printInfo[A](value: A)(implicit printer: InfoPrinter[A]): String = {
+        printer.toInfo(value)
+      }
+
+      assert("[__STRING__] hello" === printInfo("hello"))
+      assert("[__USER__] Raul, 23" === printInfo(User("Raul", 23)))
+  }
 }
