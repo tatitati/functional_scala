d33433dea156128a50b14a5950d93bb9996866f3
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-14 23:59:09 +0100

    more

--- src/main/scala/shop/PetService.scala
+++ src/main/scala/shop/PetService.scala
@@ -1,6 +1,7 @@
 package shop
 
 import cats.data.EitherT
+import cats.effect.IO
 import cats.syntax.either._
 
 class PetService(petRepository: PetRepository) {
@@ -8,7 +9,11 @@ class PetService(petRepository: PetRepository) {
   def create(pet: Pet): Either[String, Pet] = {
     for {
       _ <- petRepository.doesNotExist(pet) // return: Either[String, Unit]
+      action <- Right{IO{println("yeaaaaaaaaaaa")}}
       saved <- petRepository.create(pet)   // return: Either[String, Pet]
-    } yield saved
+    } yield {
+      action.unsafeRunSync() // I cannot mix monads, however I can extract and do shit in here
+      saved
+    }
   }
 }
