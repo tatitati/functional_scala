e9f22a4fb391749d6227a364035ecefa356234e0
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-16 00:00:28 +0100

    more

--- src/test/scala/learning/scala/TypeClassesSpec.scala
+++ src/test/scala/learning/scala/TypeClassesSpec.scala
@@ -6,21 +6,21 @@ class TypeClassesSpec extends FunSuite {
     sealed trait Animal
     final case class Dog(name: String) extends Animal
 
-
     // type class
     trait BehavesLikeHuman[A] {
       def speak(a: A): String
     }
 
-    // type class INSTANCES
-    // usually people put these implicits inside another object "<something>Instances"
+    // type class INSTANCES:
+    // (usually people put these implicits inside another object "<something>Instances")
     implicit val dogBehavesLikeHuman = new BehavesLikeHuman[Dog] {
       def speak(dog: Dog): String = {
         s"I'm a dog named: ${dog.name}"
       }
     }
 
-    // type class SYNTAX (this is the interfaces that the consumer will use)
+    // type class SYNTAX:
+    // (this is the interfaces that the consumer will use)
     // usually people put these implicits inside another object "<something>Syntax"
     implicit class BehavesLikeHumanOps[A](value: A) {
       def saySomething(implicit behavesLikeHumanInstance: BehavesLikeHuman[A]): String = {
@@ -28,7 +28,7 @@ class TypeClassesSpec extends FunSuite {
       }
     }
 
-    test("can expand behaviour") {
+    test("Type classes can add new behaviour to existing classes") {
       val rover = Dog("Rover")
       assert("I'm a dog named: Rover" === rover.saySomething)
     }
