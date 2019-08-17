deaa2d41b221225076b30297dfa3c7d1e12f4447
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-17 10:50:17 +0100

    more

--- src/test/scala/learning/scala/LiftFunctionSpec.scala
+++ src/test/scala/learning/scala/LiftFunctionSpec.scala
@@ -1,33 +1,54 @@
 package test.learning.scala
 
 import org.scalatest.FunSuite
-
 import scala.util.{Success, Try}
 
 class LiftFunctionSpec extends FunSuite {
 
-  def parseUserForm(age: String, numberOfBrothers: String): Try[String] = {
-    val optAge: Try[Int] = Try{age.toInt}
-    val optBrothers: Try[Int] = Try{numberOfBrothers.toInt}
-
-    lifter(optAge, optBrothers)(insuranceRateQuote)
-  }
-  
-  def insuranceRateQuote(age: Int, numberOfBrothers: Int): String = {
-      s"Age: $age, Brothers: $numberOfBrothers"
-  }
-
-
-  def lifter[A, B, C](a: Try[A], b: Try[B])(f: (A, B) => C): Try[C] = {
-    a flatMap (aa =>
-      b map (bb =>
-        f(aa, bb)
-        )
-      )
-  }
-
   test("check happy path") {
-      var result = parseUserForm("23", "3")
-      assert(Success("Age: 23, Brothers: 3") === result)
+    def parseUserForm(age: String, numberOfBrothers: String): Try[String] = {
+      val optAge: Try[Int] = Try{age.toInt}
+      val optBrothers: Try[Int] = Try{numberOfBrothers.toInt}
+
+      lifter(optAge, optBrothers)(insuranceRateQuote)
+    }
+
+    def insuranceRateQuote(age: Int, numberOfBrothers: Int): String = {
+      s"Age: $age, Brothers: $numberOfBrothers"
+    }
+
+    def lifter[A, B, C](a: Try[A], b: Try[B])(f: (A, B) => C): Try[C] = {
+      a flatMap (aa =>
+        b map (bb =>
+          f(aa, bb)
+          )
+        )
+    }
+
+    val result = parseUserForm("23", "3")
+    assert(Success("Age: 23, Brothers: 3") === result)
+  }
+
+  test("Using FOR{}") {
+    def parseUserForm(age: String, numberOfBrothers: String): Try[String] = {
+      val optAge: Try[Int] = Try{age.toInt}
+      val optBrothers: Try[Int] = Try{numberOfBrothers.toInt}
+
+      lifter(optAge, optBrothers)(insuranceRateQuote)
+    }
+
+    def insuranceRateQuote(age: Int, numberOfBrothers: Int): String = {
+      s"Age: $age, Brothers: $numberOfBrothers"
+    }
+
+    def lifter[A, B, C](a: Try[A], b: Try[B])(f: (A, B) => C): Try[C] = {
+      for {
+        aa <- a
+        bb <- b
+      } yield f(aa, bb)
+    }
+
+    val result = parseUserForm("23", "3")
+    assert(Success("Age: 23, Brothers: 3") === result)
   }
 }
