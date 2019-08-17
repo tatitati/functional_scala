45d0715db6765d97b1466908306162f615b615dd
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-17 10:45:03 +0100

    more

--- src/test/scala/learning/scala/LiftFunctionSpec.scala
+++ src/test/scala/learning/scala/LiftFunctionSpec.scala
@@ -6,16 +6,17 @@ import scala.util.{Success, Try}
 
 class LiftFunctionSpec extends FunSuite {
 
-  def insuranceRateQuote(age: Int, numberOfBrothers: Int): String = {
-      s"Age: $age, Brothers: $numberOfBrothers"
-  }
-
   def parseUserForm(age: String, numberOfBrothers: String): Try[String] = {
     val optAge: Try[Int] = Try{age.toInt}
     val optBrothers: Try[Int] = Try{numberOfBrothers.toInt}
 
     lifter(optAge, optBrothers)(insuranceRateQuote)
   }
+  
+  def insuranceRateQuote(age: Int, numberOfBrothers: Int): String = {
+      s"Age: $age, Brothers: $numberOfBrothers"
+  }
+
 
   def lifter[A, B, C](a: Try[A], b: Try[B])(f: (A, B) => C): Try[C] = {
     a flatMap (aa =>
