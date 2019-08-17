fa41006d2d4fc6b8aa4e0a72c3e07cc831e54d75
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-17 13:42:07 +0100

    more

---
+++ src/test/scala/learning/scala/BehaviourInjection/CatsTypeClassesSpec.scala
@@ -0,0 +1,26 @@
+package test.learning.scala.BehaviourInjection
+
+import cats.Show
+import cats.syntax.show._
+import org.scalatest.FunSuite
+import test.learning.fpModules._
+
+class CatsTypeClassesSpec extends FunSuite {
+  sealed trait Topping
+  case object Cheese extends Topping
+  case object Pepperoni extends Topping
+  case object Sausage extends Topping
+  case object Mushrooms extends Topping
+  case object Onions extends Topping
+
+
+  implicit val pizzaShow = Show.show[Pizza] { p =>
+    s"""|Pizza(${p.crustSize}, ${p.crustType}),
+        |     toppings = ${p.toppings}""".stripMargin
+  }
+
+  test("asdfasd") {
+    val p = Pizza(SmallCrustSize, RegularCrustType, Seq())
+    println(p.show)
+  }
+}
