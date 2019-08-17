4ea8b4b11f2656227b7a77fb91541685c2459a91
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-17 10:42:54 +0100

    more

---
+++ src/test/scala/learning/scala/LiftFunctionSpec.scala
@@ -0,0 +1,32 @@
+package test.learning.scala
+
+import org.scalatest.FunSuite
+
+import scala.util.{Success, Try}
+
+class LiftFunctionSpec extends FunSuite {
+
+  def insuranceRateQuote(age: Int, numberOfBrothers: Int): String = {
+      s"Age: $age, Brothers: $numberOfBrothers"
+  }
+
+  def parseUserForm(age: String, numberOfBrothers: String): Try[String] = {
+    val optAge: Try[Int] = Try{age.toInt}
+    val optBrothers: Try[Int] = Try{numberOfBrothers.toInt}
+
+    lifter(optAge, optBrothers)(insuranceRateQuote)
+  }
+
+  def lifter[A, B, C](a: Try[A], b: Try[B])(f: (A, B) => C): Try[C] = {
+    a flatMap (aa =>
+      b map (bb =>
+        f(aa, bb)
+        )
+      )
+  }
+
+  test("check happy path") {
+      var result = parseUserForm("23", "3")
+      assert(Success("Age: 23, Brothers: 3") === result)
+  }
+}
