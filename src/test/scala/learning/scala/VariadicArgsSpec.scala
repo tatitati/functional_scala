6baf3dc187016922dc3b047937f00775490f9812
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-18 14:24:56 +0100

    variadic

---
+++ src/test/scala/learning/scala/VariadicArgsSpec.scala
@@ -0,0 +1,17 @@
+package test.learning.scala
+
+import org.scalatest.FunSuite
+
+class VariadicArgsSpec extends FunSuite {
+
+  test("can pass a variant amount of arguments"){
+
+    def printAllNames(names: String*): String = {
+      var result = ""
+      names.foreach{result += _}
+      result
+    }
+
+    assert("aaabbbb" === printAllNames("aaa", "bbbb"))
+  }
+}
