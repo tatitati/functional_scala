62ffd6bf51d81f58758bb30ba4dcc96bb12c4185
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-20 00:13:49 +0100

    more

--- src/test/scala/learning/cats/CatSemigroupSpec.scala
+++ src/test/scala/learning/cats/CatSemigroupSpec.scala
@@ -1,14 +1,26 @@
 package test.learning.cats
 
 import org.scalatest.FunSuite
-import cats.Semigroup
+import cats.{Semigroup, Semigroupal}
 import cats.implicits._
 
 class CatSemigroupSpec extends FunSuite {
+
+  // Trait Semigroup[A] {
+  //    def combine(x: A, y: A): A
+  // }
 
   test("semigroup") {
     val result = Semigroup[String].combine("hi", "there")
 
     assert("hithere" == result)
+  }
+
+  test("semigroups are not semigroupal:") {
+    val result1 = Semigroup[String].combine("hi", "there")
+    val result2 = Semigroupal[Option].product(Some("hi"), Some("there"))
+
+    assert("hithere" == result1)
+    assert(Some(("hi", "there")) === result2)
   }
 }
