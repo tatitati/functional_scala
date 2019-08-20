edd07586ad1ba7d05dc17abdb4a1e529becf74d8
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-08-20 23:51:17 +0100

    some EitherT transformer

--- src/test/scala/learning/cats/MonadTransformers/EithertSpec.scala
+++ src/test/scala/learning/cats/MonadTransformers/EithertSpec.scala
@@ -1,7 +1,29 @@
 package test.learning.cats.MonadTransformers
 
+import cats.data.{EitherT}
+import cats.instances.list._
 import org.scalatest.FunSuite
 
 class EithertSpec extends FunSuite{
+  test("basic") {
+    val myOptionList: List[Option[Int]] = List(
+      None,
+      Some(2),
+      Some(3),
+      None,
+      Some(5)
 
+    )
+    val result = EitherT.fromOptionF(myOptionList, "option not defined")
+
+    assert(
+      EitherT(List(
+        Left("option not defined"),
+        Right(2),
+        Right(3),
+        Left("option not defined"),
+        Right(5))
+      ) == result
+    )
+  }
 }
