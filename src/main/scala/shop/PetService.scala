47bb8d2075dc5278c2fa222cbf85084415c14628
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-09-04 19:43:22 +0100

    more

--- src/main/scala/shop/PetService.scala
+++ src/main/scala/shop/PetService.scala
@@ -8,12 +8,8 @@ class PetService(petRepository: PetRepository) {
 
   def create(pet: Pet): Either[String, Pet] = {
     for {
-      _ <- petRepository.doesNotExist(pet) // return: Either[String, Unit]
-      action <- Right{IO{println("yeaaaaaaaaaaa")}}
-      saved <- petRepository.create(pet)   // return: Either[String, Pet]
-    } yield {
-      action.unsafeRunSync() // I cannot mix monads, however I can extract and do shit in here
-      saved
-    }
+      _ <- petRepository.exist(pet)
+      saved <- petRepository.create(pet)  
+    } yield saved
   }
 }
