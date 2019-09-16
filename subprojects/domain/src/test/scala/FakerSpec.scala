b010729a6ae37fd253158f738b74ce9df1777d7a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-09-16 23:03:15 +0100

    some new tests

---
+++ subprojects/domain/src/test/scala/FakerSpec.scala
@@ -0,0 +1,32 @@
+package domain.test
+
+import cats.data.State
+import domain.order.OrderId
+import domain.test.pet.BuilderPetOps.BuilderState
+import org.scalatest.FunSuite
+
+class FakerSpec extends FunSuite {
+
+  test("Explain how to use state in builder"){
+    val result = Faker.positiveInt()
+
+    val state = result.run(SeedLong(10)).value
+
+    assert((SeedLong(-8702919015481313007L),1254031283) == state)
+  }
+
+  test("Understand a bit better State") {
+    val anInt: State[SeedLong, Int] = Faker.positiveInt()
+
+    val createPet: State[SeedLong, BuilderState] = for {
+      age <- anInt
+      price <- anInt
+    } yield BuilderState(OrderId("any_id"), age, "any name", price)
+
+    assert(
+      (
+        SeedLong(3854786773771191933L),
+        BuilderState(OrderId("any_id"),542183646,"any name",584218809)
+      ) === createPet.run(SeedLong(2323)).value)
+  }
+}
