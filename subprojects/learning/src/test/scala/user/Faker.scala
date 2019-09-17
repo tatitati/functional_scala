f27a8744a15172d8528b4f0497a41a0c0296316f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-09-17 20:51:36 +0100

    more

--- subprojects/learning/src/test/scala/user/Faker.scala
+++ subprojects/learning/src/test/scala/user/Faker.scala
@@ -4,26 +4,18 @@ import scala.util.Random
 import com.github.nscala_time.time.Imports.DateTime
 
 object Faker {
-  def anyOf[T](items: T*): T = {
-    items(Random.nextInt(items.length))
-  }
+  def anyOf[T](items: T*): T = items(Random.nextInt(items.length)
 
   def text(length: Int = 10): String = {
     val value = for(i <- 1 to length) yield { Random.nextPrintableChar() }
     value.mkString
   }
 
-  def boolean: Boolean = {
-    Random.nextBoolean()
-  }
+  def boolean: Boolean = Random.nextBoolean()
 
-  def int: Int = {
-    Random.nextInt()
-  }
+  def int: Int = Random.nextInt()
 
-  def long: Long = {
-    Random.nextLong()
-  }
+  def long: Long = Random.nextLong()
 
   def date: DateTime = {
     anyOf(
