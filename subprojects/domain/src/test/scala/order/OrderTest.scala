fbd89b2d4bcb980de84e4e5b23cd9770ae327ebe
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-09-23 21:55:05 +0100

    more

--- subprojects/domain/src/test/scala/order/OrderTest.scala
+++ subprojects/domain/src/test/scala/order/OrderTest.scala
@@ -3,14 +3,26 @@ package SeparateDataFromBehaviour.test.order
 import org.scalatest.FunSuite
 import com.github.nscala_time.time.Imports._
 import SeparateDataFromBehaviour.order.{DEBITCARD, Order}
+import cats.effect.IO
 
 class OrderTest extends FunSuite {
 
   test("I can create an order"){
-    val order = Order(
-      date = DateTime.now,
-      medium = DEBITCARD,
-      country = "France"
-    )
+    // WRONG:
+    //
+    //    val order = Order(
+    //      date = DateTime.now,
+    //      medium = DEBITCARD,
+    //      country = "France"
+    //    )
+    
+    val build: IO[Order] = for {
+      date <- IO{ DateTime.now }
+      medium <- IO(DEBITCARD)
+      country <- IO("france")
+    } yield Order(date, medium, country)
+
+    val anOrder = build.unsafeRunSync()
+    assert(anOrder.isInstanceOf[Order])
   }
 }
