08340c77026ab6b8c6a14f2fc5aa9dfbfa534ea7
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-01 22:18:21 +0100

    added basic connection....I have to dockerize postgres!

--- subprojects/learning/src/test/scala/doobie/DoobieSpec.scala
+++ subprojects/learning/src/test/scala/doobie/DoobieSpec.scala
@@ -1,7 +1,29 @@
 package learning.test.doobie
 
 import org.scalatest.FunSuite
+import doobie._
+import cats._
+import cats.effect._
+import cats.implicits._
+import doobie.implicits._
+import doobie.util.ExecutionContexts
 
 class DoobieSpec extends FunSuite {
 
+  test("asdfasd"){
+    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+
+    // A transactor that gets connections from java.sql.DriverManager and executes blocking operations
+    // on an our synchronous EC. See the chapter on connection handling for more info.
+    val xa = Transactor.fromDriverManager[IO](
+      "org.postgresql.Driver",     // driver classname
+      "jdbc:postgresql:doobie_db",     // connect URL (driver-specific)
+      "postgres",                       // user
+      "1234",                           // password
+      Blocker.liftExecutionContext(ExecutionContexts.synchronous) // just for testing
+    )
+    val program1 = 42.pure[ConnectionIO]
+    val io = program1.transact(xa)
+    println(io.unsafeRunSync)
+  }
 }
