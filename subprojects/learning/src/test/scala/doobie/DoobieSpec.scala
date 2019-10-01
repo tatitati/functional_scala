836ac9cf1c53ce9a7dec54393636f55c350a79bf
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-01 22:25:49 +0100

    more

--- subprojects/learning/src/test/scala/doobie/DoobieSpec.scala
+++ subprojects/learning/src/test/scala/doobie/DoobieSpec.scala
@@ -9,20 +9,27 @@ import doobie.implicits._
 import doobie.util.ExecutionContexts
 
 class DoobieSpec extends FunSuite {
+  implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
 
-  test("Connect doobie to db"){
-    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+  val xa = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver",
+    "jdbc:postgresql:doobie_db",
+    "postgres",
+    "1234",
+    Blocker.liftExecutionContext(ExecutionContexts.synchronous)
+  )
 
-    val xa = Transactor.fromDriverManager[IO](
-      "org.postgresql.Driver",
-      "jdbc:postgresql:doobie_db",
-      "postgres",
-      "1234",
-      Blocker.liftExecutionContext(ExecutionContexts.synchronous)
-    )
+  test("ConnectionIO is converted into IO"){
     val program1 = 42.pure[ConnectionIO]
-    val io = program1.transact(xa)
+    val io: IO[Int] = program1.transact(xa)
 
     assert(42 == io.unsafeRunSync)
+  }
+
+  test("Request to db simple operation") {
+    val program2: ConnectionIO[Int] = sql"select 42".query[Int].unique
+    val io2: IO[Int] = program2.transact(xa)
+
+    assert(42 == io2.unsafeRunSync)
   }
 }
