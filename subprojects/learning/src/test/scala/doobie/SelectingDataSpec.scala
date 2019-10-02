e0e1c912866e68c74f70ab5f6123c2fc1c5312ee
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-02 23:09:14 +0100

    added some rows stream in selects by using fs2 streams

--- subprojects/learning/src/test/scala/doobie/SelectingDataSpec.scala
+++ subprojects/learning/src/test/scala/doobie/SelectingDataSpec.scala
@@ -31,15 +31,47 @@ class SelectingDataSpec extends FunSuite with CustomDbConnection {
     assert(42 == result)
   }
 
-  test("can select"){
-    val result = sql"select name from country"
+  test("Can fetch ALL the rows"){
+    val result1: List[String] = sql"select name from country"
       .query[String]    // Query0[String]
       .to[List]         // ConnectionIO[List[String]]
       .transact(xa)     // IO[List[String]]
       .unsafeRunSync    // List[String]
-      .take(5)          // List[String]
 
-    assert(List("spain", "italy", "France") == result)
+
+    assert(List("spain", "italy", "France") == result1) // we read by default all the rows
+
+    val result2 = result1.take(2)
+    assert(List("spain", "italy") == result2)
   }
 
+  test("Can avoid fetching all the rows with streams") {
+    val result: List[String] = sql"select name from country"
+      .query[String]    // Query0[String]
+      .stream           // Stream[ConnectionIO, String]
+      .take(2)          // Stream[ConnectionIO, String]
+      .compile.toList   // ConnectionIO[List[String]]
+      .transact(xa)     // IO[List[String]]
+      .unsafeRunSync    // List[String]
+
+    assert(List("spain", "italy") == result)
+  }
+
+  test("multi column select") {
+    val y = xa.yolo // a stable reference is required
+    import y._
+
+    val result = sql"select code, name, population, gnp from country"
+      .query[(String, String, Int, Option[Double])]
+      .stream
+      .take(2)
+      .compile.toList
+      .quick
+      .unsafeRunSync
+
+    assert(
+      List(("SP" ,"spain",500,Some(8.0)), ("IT" ,"italy",600,Some(3.0)))
+      == result
+    )
+  }
 }
